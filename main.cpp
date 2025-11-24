45c02e3bbad890e050856cd5cb313f183d8df8ac
FShaaM <zinovevas135@gmail.com>
2025-11-24 12:47:58 +0300

    Commit 2 (add Planar)

--- main.cpp
+++ main.cpp
@@ -1,4 +1,26 @@
+#include <iostream>
+
+struct Planar
+{
+	virtual int x() const;
+	virtual int y() const;
+	virtual int abc_sqr() const;
+	virtual ~Planar() = default;
+};
+
+Planar* make(std::istream& is);
+Planar* most_left(Planar** pls, size_t k);
+
 int main()
 {
+	Planar* pls[10] = {};
+	size_t k = 0;
 
+	//pls[k++] = make(std::cin);
+
+	Planar* left = most_left(pls, k);
+
+	//drow(left);
+
+	// free_planars(pls, k);
 }
