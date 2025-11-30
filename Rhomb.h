c117851e402c27d2e834efde7878de9972a16a1e
FShaaM <zinovevas135@gmail.com>
2025-11-30 16:27:32 +0300

    Added Rhomb

--- Rhomb.h
+++ Rhomb.h
@@ -2,7 +2,18 @@
 
 #define RHOMB_H
 
+#include "Planar.h"
+#include "Point.h"
 
+struct Rhomb : Planar
+{
+  virtual int x() const;
+  virtual int y() const;
+  virtual int abc_sqr() const;
 
+  Rhomb();
+  Rhomb(Point center, int diag1, int diag2);
+  Rhomb(Point p1, Point p2, Point p3, Point p4);
+}
 
 #endif
